2d065c3d092094fc4713169a45b4322ceb5b0cc3
Ivan Reunov <riva@bp-solutions.ru>
2021-08-03 11:27:23 +0300

    Обложка митапа.

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,10 +1,33 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
+import {defineComponent} from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    image: {
+      type: String,
+      required: false
+    }
+  },
+
+  computed: {
+    coverStyle() {
+      let coverStyle = {};
+
+      if (this.image) {
+        coverStyle['--bg-url'] = 'url(\'' + this.image + '\')';
+      }
+
+      return coverStyle;
+    }
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="coverStyle">
+    <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
